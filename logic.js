d3ef468f94a1c71b5d094b1295f98de8a14bfdb4
Mitchell Simms <mitchell_simms@outlook.com>
2023-06-09 12:25:40 -0600

    javascript is working, both json files are loaded. Added style and 3 functions but still need to figure out how to integrate spotify.json into the functions.

--- logic.js
+++ logic.js
@@ -10,54 +10,92 @@ var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.geoJson(geojson).addTo(map);
+// Create a function to add color to the map
+// Sourced from https://leafletjs.com/examples/choropleth/
+function getColor(d) {
+    return d > 1000 ? '#fff7fb' :
+           d > 500  ? '#ece2f0' :
+           d > 200  ? '#d0d1e6' :
+           d > 100  ? '#a6bddb' :
+           d > 50   ? '#67a9cf' :
+           d > 20   ? '#3690c0' :
+           d > 10   ? '#02818a' :
+                      '#016450';
+}
 
+// Add style to the map and call color function
+// Sourced from https://leafletjs.com/examples/choropleth/
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'black',
+        dashArray: '1',
+        fillOpacity: 0.75
+    };
+}
+// highlight function when mouse is hovered over the state 
+function highlightFeature(e) {
+    var layer = e.target;
 
-// function states(data){
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.5
+    });
 
-//}
-
-// read in the data for the state boarders. 
+    layer.bringToFront();
+}
+// Reset highlight feature with original style 
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+// function to zoom in on state when clicked 
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+// Function to call each style function when the layer is used. 
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+// Create a geoJSON layer to call the style and color function. 
 var geojson;
-
 d3.json("data.json", function(error, data) {
     if (error) {
-      console.error(error);
+        console.error(error);
     } else {
-      // Handle the data
-      geojson= data;
-      console.log(geojson.features[0].geometry.coordinates);
+        // Handle the data
+        geojson= data;
+        // Call the style and get color function to add color and style to geojson layer
+        L.geoJSON(geojson, {style: style}).addTo(map);
+        geojson= L.geoJson(geojson, {
+            style: style,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+    }});
 
-      L.choropleth(geojson.features[0].geometry.coordinates, {
+// Create a function to diplay the top songs when mouse hovers over state. 
+function topSongs(spotifyData){
+    console.log(spotifyData)
+}
 
-        // Define which property in the features to use.
-        valueProperty: "DP03_16E",
-    
-        // Set the color scale.
-        scale: ["#ffffb2", "#b10026"],
-    
-        // The number of breaks in the step range
-        steps: 10,
-    
-        // q for quartile, e for equidistant, k for k-means
-        mode: "q",
-        style: {
-          // Border color
-          color: "#fff",
-          weight: 1,
-          fillOpacity: 0.8
+// Load in the spotify json. 
+// when pushed to main, fix file path 
+d3.json("spotify_data.json", function(error, spotifyData) {
+    if (error) {
+        console.error(error);
+    } else {
+        // Handle the data
+        // call the function 
+        topSongs(spotifyData);
 
-    }}).addTo(map);
-}});
-  
-
-// spotify_json_url= ''
-
-// // Perform a d3 get request on the url and store the response in 'data'
-// d3.json(spotify_json_url).then(function(data) {
-//     // call the earthquakeFeatures function with the data
-//     states(json_data);
-// });
+    }});
 
 
 // add legend to show what the scale is for depth of the earthquake. 
