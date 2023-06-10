f74df025d2af16f4b96be5f54d71e26d99008830
Mitchell Simms <mitchell_simms@outlook.com>
2023-06-10 12:40:42 -0600

    almost there

--- logic.js
+++ logic.js
@@ -47,15 +47,20 @@ function highlightFeature(e) {
     });
 
     layer.bringToFront();
+    info.update(layer.feature.properties.name);
 }
+
 // Reset highlight feature with original style 
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
+    info.update();
 }
+
 // function to zoom in on state when clicked 
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
+
 // Function to call each style function when the layer is used. 
 function onEachFeature(feature, layer) {
     layer.on({
@@ -64,7 +69,37 @@ function onEachFeature(feature, layer) {
         click: zoomToFeature
     });
 }
-// Create a geoJSON layer to call the style and color function. 
+
+// create function geoFormat to take the states names to be able to compare to the states names
+// from the other json file (data.json)
+// function geoFormat(data){
+//     for (var i=0; i< data.length; i++){
+//         console.log(data)}
+//         // check to make sure the states names match 
+//         if (data[i].features.properties.name === state[i]){
+//             // if they do match, bind popup to each state when hovered over. 
+//             var name= state[i];
+//         }
+// }
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>States Top Tracks</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' 
+        : 'Hover over a state');
+};
+
+info.addTo(map);
+
+//  Create a geoJSON layer to call the style and color function. 
 var geojson;
 d3.json("data.json", function(error, data) {
     if (error) {
@@ -72,17 +107,21 @@ d3.json("data.json", function(error, data) {
     } else {
         // Handle the data
         geojson= data;
+        console.log(geojson)
         // Call the style and get color function to add color and style to geojson layer
-        L.geoJSON(geojson, {style: style}).addTo(map);
         geojson= L.geoJson(geojson, {
             style: style,
             onEachFeature: onEachFeature
         }).addTo(map);
-    }});
+}});
 
 // Create a function to diplay the top songs when mouse hovers over state. 
 function topSongs(spotifyData){
-    console.log(spotifyData)
+    for (var i=0; i< spotifyData.length; i++){
+        const state= (spotifyData[i].State)
+        const topTracks= (spotifyData[i].Track_Artist_Popularity)
+    }
+    
 }
 
 // Load in the spotify json. 
